482091c84951882f603d6026fae326446d017263
krsk29 <103282536+krsk29@users.noreply.github.com>
2023-10-05 22:46:33 +0000

    constants file to keep track of all constant variables defined in project. Easier to import variables that way

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+import os
+
+#get the project directory path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#data
+DATA = os.path.join(BASE_DIR, 'data')
+
+LOGISTICS_DATA = os.path.join(DATA, 'logistics_data.csv')
+MATERIALS_DATA = os.path.join(DATA, 'materials_data.csv')
+PROJECTS_DATA = os.path.join(DATA, 'projects_data.csv')
+SUPPLIERS_DATA = os.path.join(DATA, 'suppliers_data.csv')
