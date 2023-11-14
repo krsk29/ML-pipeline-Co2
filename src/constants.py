740f8ae589cd4e5462baf4531f8cc942ba0eebae
krsk29 <103282536+krsk29@users.noreply.github.com>
2023-11-14 17:50:28 +0000

    added new MODELS directory to constants

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,10 @@ import os
 #get the project directory path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#models
+MODELS = os.path.join(BASE_DIR, 'models')
+RANDOM_FOREST_MODEL = os.path.join(MODELS, 'random_forest')
+
 #data
 DATA = os.path.join(BASE_DIR, 'data')
 
@@ -12,7 +16,8 @@ PROJECTS_DATA = os.path.join(DATA, 'projects_data.csv')
 SUPPLIERS_DATA = os.path.join(DATA, 'suppliers_data.csv')
 FINAL_DATA_parquet = os.path.join(DATA, 'final_df.parquet')
 FINAL_DATA_csv = os.path.join(DATA, 'final_df.csv')
-# pre-processed data
+
+# pre-processed data for training
 TEST_TRANSFORMED_DATA = os.path.join(DATA, 'test_transformed.parquet')
 TRAIN_TRANSFORMED_DATA = os.path.join(DATA, 'train_transformed.parquet')
 
