e1d501272ed091d7f8a458f9d01f4fde19b62153
krsk29 <103282536+krsk29@users.noreply.github.com>
2024-02-23 00:05:43 +0000

    refactoring modelling to run multiple model runs in an experiment

--- src/modelling.py
+++ src/modelling.py
@@ -1,13 +1,15 @@
 
 import os
-print("Current working directory:", os.getcwd())
 import logging
 
 from pyspark.sql import SparkSession
 
-from pyspark.ml.regression import RandomForestRegressor, LinearRegression
+from pyspark.ml.regression import RandomForestRegressor, LinearRegression, GBTRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+
+import mlflow
+import mlflow.spark
 
 from dotenv import load_dotenv
 from script_utils import get_env_variable, configure_logging
@@ -28,37 +30,76 @@ load_dotenv()
 logs_dir = get_env_variable('LOGS_DIR')
 configure_logging(logs_dir, 'modelling')
 
-if __name__ == "__main__":
+def train_linear_regression(TEST_TRANSFORMED_DF, TRAIN_TRANSFORMED_DF):
+    # start mlflow run for linear regression
+    with mlflow.start_run():
+        mlflow.set_tag("model", "Linear Regression")
+
+        # Model training and evaluation
+        lr = LinearRegression(featuresCol="features", labelCol="log_CO2_emission")
+        lr_model = lr.fit(TRAIN_TRANSFORMED_DF)
+        lr_predictions = lr_model.transform(TEST_TRANSFORMED_DF)
+
+        lr_evaluator = RegressionEvaluator(labelCol="log_CO2_emission", predictionCol="prediction", metricName="rmse")
+        lr_rmse = lr_evaluator.evaluate(lr_predictions)
+        lr_r2 = lr_evaluator.evaluate(lr_predictions, {lr_evaluator.metricName: "r2"})
+
+        # Log params, metrics, and model
+        mlflow.log_param("model_type", "Linear Regression")
+        mlflow.log_metric("rmse", lr_rmse)
+        mlflow.log_metric("r2", lr_r2)
+        mlflow.spark.log_model(lr_model, "linear_regression_model") 
+
+def train_gradient_boosted_trees(TRAIN_TRANSFORMED_DF, TEST_TRANSFORMED_DF):
+    # Start MLflow run for Gradient Boosted Trees
+    with mlflow.start_run():
+        mlflow.set_tag("model", "Gradient Boosted Trees")
+
+        # Model training and evaluation
+        gbt = GBTRegressor(featuresCol="features", labelCol="log_CO2_emission")
+        gbt_paramGrid = (ParamGridBuilder()
+                         .addGrid(gbt.maxDepth, [2, 3])
+                         .addGrid(gbt.maxBins, [3])
+                         .addGrid(gbt.maxIter, [4])
+                         .build())
+
+        gbt_evaluator = RegressionEvaluator(labelCol="log_CO2_emission", predictionCol="prediction", metricName="rmse")
+        gbt_cv = CrossValidator(estimator=gbt,
+                                estimatorParamMaps=gbt_paramGrid,
+                                evaluator=gbt_evaluator,
+                                numFolds=3)
+
+        gbt_cv_model = gbt_cv.fit(TRAIN_TRANSFORMED_DF)
+        gbt_cv_predictions = gbt_cv_model.transform(TEST_TRANSFORMED_DF)
+
+        gbt_cv_rmse = gbt_evaluator.evaluate(gbt_cv_predictions)
+        gbt_cv_r2 = gbt_evaluator.evaluate(gbt_cv_predictions, {gbt_evaluator.metricName: "r2"})
+
+        # Log params, metrics, and model
+        mlflow.log_param("model_type", "Gradient Boosted Trees")
+        mlflow.log_metric("rmse", gbt_cv_rmse)
+        mlflow.log_metric("r2", gbt_cv_r2)
+        mlflow.spark.log_model(gbt_cv_model, "gbt_model")
+
+def main():
+    # MLflow experiment setup
+    mlflow.set_experiment(experiment_name="CO2 Emission Models")
 
     spark = SparkSession.builder \
         .master("local[*]") \
         .appName("CO2 Emission ML Pipeline - Modelling") \
         .getOrCreate()
 
-    #reading df
+    # Data preparation
     TEST_TRANSFORMED_DF = spark.read.parquet(test_data_file_path)
     TRAIN_TRANSFORMED_DF = spark.read.parquet(train_data_file_path)
 
-    # initialize the Linear Regression model
-    lr = LinearRegression(featuresCol="features", labelCol="log_CO2_emission")
+    # Train and log Linear Regression model
+    train_linear_regression(TRAIN_TRANSFORMED_DF, TEST_TRANSFORMED_DF)
 
-    # train model
-    lr_model = lr.fit(TRAIN_TRANSFORMED_DF)
+    # Train and log Gradient Boosted Trees model
+    train_gradient_boosted_trees(TRAIN_TRANSFORMED_DF, TEST_TRANSFORMED_DF)
 
-    # predict on the test data
-    lr_predictions = lr_model.transform(TEST_TRANSFORMED_DF)
-
-    # evaluate the model
-    lr_evaluator = RegressionEvaluator(labelCol="log_CO2_emission", predictionCol="prediction", metricName="rmse")
-    lr_rmse = lr_evaluator.evaluate(lr_predictions)
-    print(f"Root Mean Squared Error (RMSE) on test data = {lr_rmse}")
-
-    lr_evaluator = RegressionEvaluator(labelCol="log_CO2_emission", predictionCol="prediction", metricName="r2")
-    lr_r2 = lr_evaluator.evaluate(lr_predictions)
-    print(f"R-squared on test data = {lr_r2}")
-
-
-
-
-
-
+    
+if __name__ == "__main__":
+    main()
